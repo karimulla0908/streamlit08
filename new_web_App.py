cd4e670d4eeb2537273fa56bbe25fc9324f6a173
karimulla0908 <168703235+karimulla0908@users.noreply.github.com>
2024-05-20 13:49:32 +0530

    Update new_web_App.py

--- new_web_App.py
+++ new_web_App.py
@@ -13,7 +13,7 @@ st.markdown("<h1 class='title'>Sample Variance as an Unbiased Estimator of Popul
 
 st.sidebar.header("Parameters")
 
-st.sidebar.markdown("<div class='section-title'>Population Parameters</div>", unsafe_allow_html=True)
+
 st.sidebar.markdown("<div class='section-title'>Population Size</div>", unsafe_allow_html=True)
 
 population_size = 10000
@@ -22,7 +22,7 @@ sample_size = st.sidebar.number_input("Sample size", min_value=10, max_value=100
 
 ddof1_sample = st.sidebar.number_input("Degree of freedom", min_value=0, max_value=10, value=1, step=1)
 
-st.sidebar.markdown("<div class='section-title'>Sampling Parameters</div>", unsafe_allow_html=True)
+st.sidebar.markdown("<div class='section-title'>Sample Parameters</div>", unsafe_allow_html=True)
 no_of_samples = st.sidebar.number_input("Number of samples", min_value=10, max_value=10000, value=10, step=1)
 alpha = 0.05
 
